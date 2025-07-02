7369a8647c0ec7faa4da8720bf78e8d1b1fafee4
devanshi <parekhdevanshi76@gmail.com>
2025-07-02 10:34:39 +0530

    update

--- employee_salary.js
+++ employee_salary.js
@@ -1,27 +1,32 @@
 let e_name = "Sunny";
 let e_id = 7;
 let basicsalary = 45000;
+let exp = 2;
+let bouns = 0;
 
-let hra = basicsalary * 0.20;
-let da = basicsalary * 0.10;
-let grossSalary = basicsalary + hra + da;
-let tax;
-
-if (grossSalary >= 50000) {
-    tax = grossSalary * 0.20;
-} else if (grossSalary >= 40000) {
-    tax = grossSalary * 0.15;
-} else if (grossSalary >= 30000) {
-    tax = grossSalary * 0.10;
-} else {
-    tax = grossSalary * 0.05;
+if(exp >= 5 && basicsalary >= 50000){
+    bouns = basicsalary * 0.15;
+}else if(exp >= 3 && basicsalary >= 30000){
+    bouns = basicsalary * 0.10;
+}else if(exp >= 1.5 && basicsalary >= 20000){
+    bouns = basicsalary * 0.08;
+}else{
+    bouns = 0;
 }
 
-let netSalary = grossSalary - tax;
+let HRA = basicsalary * 0.5;
+let MA = basicsalary * 0.05;
+let TA = basicsalary * 0.2;
+let DA = basicsalary * 0.1;
+let PF = basicsalary * 0.12;
+let PT = 200;
+
+let grossSalary = basicsalary + HRA + DA + TA + MA + bouns;
+let netSalary = grossSalary - PF - PT;
 
 console.log(`Employee name :    ${e_name}`);
 console.log(`Employee ID :      ${e_id}`);
-console.log(`Basic salary :     ${basicsalary}`);
+console.log(`Basic salary :     ₹${basicsalary}`);
+console.log(`Bouns :            ₹${bouns}`);
 console.log(`Gross Salary:      ₹${grossSalary.toFixed(2)}`);
-console.log(`Tax Deducted:      ₹${tax.toFixed(2)}`);
 console.log(`Net Salary:        ₹${netSalary.toFixed(2)}`);
